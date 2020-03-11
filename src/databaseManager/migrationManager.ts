ced50fd55a732378621d115448a6082313316dec
Vasily Novosad <delhsmail@gmail.com>
2020-03-11 19:44:23 +0500

    MigrationManager code review

--- src/databaseManager/migrationManager.ts
+++ src/databaseManager/migrationManager.ts
@@ -7,52 +7,75 @@ const migrationManager = (config: IConfig) => {
   const knex = knexProvider(config);
 
   return {
-    up: async (migratorConfig?: MigratorConfig) => {
+    status: async (migratorConfig?: MigratorConfig) => {
       try {
-        await knex.migrate.up(migratorConfig);
-        logger.sql.debug('The UP migration was successful');
+        const result = await knex.migrate.status(migratorConfig);
+        return result;
+      } catch (err) {
+        throw new ServerError('Unknown error with Knex migration status', err);
+      }
+    },
+    currentVersion: async (migratorConfig?: MigratorConfig) => {
+      try {
+        const result = await knex.migrate.currentVersion(migratorConfig);
+        logger.sql.debug('The Knex CURRENTVERSION migration was successful');
+        return result;
       } catch (err) {
         throw new ServerError('The UP migration was failed', err);
+      }
+    },
+    up: async (migratorConfig?: MigratorConfig) => {
+      try {
+        const result = await knex.migrate.up(migratorConfig);
+        logger.sql.debug('The Knex UP migration was successful');
+        return result;
+      } catch (err) {
+        throw new ServerError('The Knex UP migration was failed', err);
       }
     },
     down: async (migratorConfig?: MigratorConfig) => {
       try {
-        await knex.migrate.down(migratorConfig);
-        logger.sql.debug('The DOWN migration was successful');
+        const result = await knex.migrate.down(migratorConfig);
+        logger.sql.debug('The Knex DOWN migration was successful');
+        return result;
       } catch (err) {
-        throw new ServerError('The DOWN migration was failed', err);
+        throw new ServerError('The Knex DOWN migration was failed', err);
       }
     },
     latest: async (migratorConfig?: MigratorConfig) => {
       try {
-        await knex.migrate.latest(migratorConfig);
-        logger.sql.debug('The LATEST migration was successful');
+        const result = await knex.migrate.latest(migratorConfig);
+        logger.sql.debug('The Knex LATEST migration was successful');
+        return result;
       } catch (err) {
-        throw new ServerError('The LATEST migration was failed', err);
+        throw new ServerError('The Knex LATEST migration was failed', err);
       }
     },
     rollback: async (migratorConfig?: MigratorConfig) => {
       try {
-        await knex.migrate.rollback(migratorConfig);
-        logger.sql.debug('The ROLLBACK migration was successful');
+        const result = await knex.migrate.rollback(migratorConfig);
+        logger.sql.debug('The Knex ROLLBACK migration was successful');
+        return result;
       } catch (err) {
-        throw new ServerError('The ROLLBACK migration was failed', err);
+        throw new ServerError('The Knex ROLLBACK migration was failed', err);
       }
     },
     list: async (migratorConfig?: MigratorConfig) => {
       try {
-        await knex.migrate.list(migratorConfig);
-        logger.sql.debug('The UP migration was successful');
+        const result = await knex.migrate.list(migratorConfig);
+        logger.sql.debug('The Knex UP migration was successful');
+        return result;
       } catch (err) {
-        throw new ServerError('The UP migration was failed', err);
+        throw new ServerError('The Knex UP migration was failed', err);
       }
     },
     make: async (name: string, migratorConfig?: MigratorConfig) => {
       try {
-        await knex.migrate.make(name, migratorConfig);
-        logger.sql.debug('The MAKE migration was successful');
+        const result = await knex.migrate.make(name, migratorConfig);
+        logger.sql.debug('The Knex MAKE migration was successful');
+        return result;
       } catch (err) {
-        throw new ServerError('The MAKE migration was failed', err);
+        throw new ServerError('The Knex MAKE migration was failed', err);
       }
     },
   };
