df985d1e14333174af73c0cfe5fe9a624092841d
Vasily Novosad <delhsmail@gmail.com>
2020-03-11 21:03:29 +0500

    Knex migrations

--- src/utils/migrate.ts
+++ src/utils/migrate.ts
@@ -12,34 +12,63 @@ enum IMigrateTo {
   up = 'up',
   down = 'down',
   latest = 'latest',
+  current = 'current',
 }
 
 interface IArgs {
   to?: IMigrateTo;
   make?: string;
+  status?: boolean;
+  list?: boolean;
 }
 
 /**
- * UP DOWN and LATEST
+ * UP DOWN LATEST and CURRENT
  */
 switch (args.to) {
   case IMigrateTo.up:
-    MigrationManager.up().then(() => {
-      console.log(chalk.green('UP migration success'));
-      process.exit(0);
-    });
+    MigrationManager.up()
+      .then(() => {
+        console.log(chalk.green('UP migration success'));
+        process.exit(0);
+      })
+      .catch(err => {
+        console.log(chalk.red('UP migration failed'), err);
+        process.exit(1);
+      });
     break;
   case IMigrateTo.down:
-    MigrationManager.down().then(() => {
-      console.log(chalk.green('DOWN migration success'));
-      process.exit(0);
-    });
+    MigrationManager.down()
+      .then(() => {
+        console.log(chalk.green('DOWN migration success'));
+        process.exit(0);
+      })
+      .catch(err => {
+        console.log(chalk.red('DOWN migration failed'), err);
+        process.exit(1);
+      });
     break;
   case IMigrateTo.latest:
-    MigrationManager.latest().then(() => {
-      console.log(chalk.green('LATEST migration success'));
-      process.exit(0);
-    });
+    MigrationManager.latest()
+      .then(() => {
+        console.log(chalk.green('LATEST migration success'));
+        process.exit(0);
+      })
+      .catch(err => {
+        console.log(chalk.red('LATEST migration failed'), err);
+        process.exit(1);
+      });
+    break;
+  case IMigrateTo.current:
+    MigrationManager.currentVersion()
+      .then(() => {
+        console.log(chalk.green('CURRENT VERSION migration success'));
+        process.exit(0);
+      })
+      .catch(err => {
+        console.log(chalk.red('CURRENT VERSION migration failed'), err);
+        process.exit(1);
+      });
     break;
 
   default:
@@ -55,9 +84,43 @@ if (args.make !== undefined) {
     process.exit(1);
   }
 
-  MigrationManager.make(args.make).then(a => {
-    console.log(chalk.green('MAKE migration success'));
-    console.log(a);
-    process.exit(0);
-  });
+  MigrationManager.make(args.make)
+    .then(() => {
+      console.log(chalk.green('MAKE migration success'));
+      process.exit(0);
+    })
+    .catch(err => {
+      console.log(chalk.red('MAKE migration failed'), err);
+      process.exit(1);
+    });
+}
+
+/**
+ * Status
+ */
+if (typeof args.status === 'boolean') {
+  MigrationManager.status()
+    .then(status => {
+      console.log(status);
+      process.exit(0);
+    })
+    .catch(err => {
+      console.log(chalk.red('get migration status failed'), err);
+      process.exit(1);
+    });
+}
+
+/**
+ * List
+ */
+if (typeof args.list === 'boolean') {
+  MigrationManager.status()
+    .then(list => {
+      console.log(list);
+      process.exit(0);
+    })
+    .catch(err => {
+      console.log(chalk.red('get migration list failed'), err);
+      process.exit(1);
+    });
 }
