6e25e1edddfcff707663d33904a7941aa7bf80e2
Vasily Novosad <delhsmail@gmail.com>
2020-03-11 19:45:02 +0500

    Dev build

--- dist/databaseManager/migrationManager.d.ts
+++ dist/databaseManager/migrationManager.d.ts
@@ -1,12 +1,14 @@
 import { MigratorConfig } from 'knex';
 import { IConfig } from './databaseManager';
 declare const migrationManager: (config: IConfig) => {
-    up: (migratorConfig?: MigratorConfig) => Promise<void>;
-    down: (migratorConfig?: MigratorConfig) => Promise<void>;
-    latest: (migratorConfig?: MigratorConfig) => Promise<void>;
-    rollback: (migratorConfig?: MigratorConfig) => Promise<void>;
-    list: (migratorConfig?: MigratorConfig) => Promise<void>;
-    make: (name: string, migratorConfig?: MigratorConfig) => Promise<void>;
+    status: (migratorConfig?: MigratorConfig) => Promise<number>;
+    currentVersion: (migratorConfig?: MigratorConfig) => Promise<string>;
+    up: (migratorConfig?: MigratorConfig) => Promise<any>;
+    down: (migratorConfig?: MigratorConfig) => Promise<any>;
+    latest: (migratorConfig?: MigratorConfig) => Promise<any>;
+    rollback: (migratorConfig?: MigratorConfig) => Promise<any>;
+    list: (migratorConfig?: MigratorConfig) => Promise<any>;
+    make: (name: string, migratorConfig?: MigratorConfig) => Promise<string>;
 };
 export default migrationManager;
 export { migrationManager };
