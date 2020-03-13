08e5ef35b0b208009cbd6fee50677bc141e899c2
Vasily Novosad <delhsmail@gmail.com>
2020-03-13 23:27:33 +0500

    GraphQL connection types was added

--- src/utils/generateCursorBundle.ts
+++ src/utils/generateCursorBundle.ts
@@ -1,11 +1,29 @@
+export enum IDirectionRange {
+  ASC = 'ASC',
+  DESC = 'DESC',
+}
+
+/**
+ * Convert string to cursor base64 string
+ * @param  {string} str
+ */
 export const stringToCursor = (str: string) => {
   return Buffer.from(str, 'binary').toString('base64');
 };
 
+/**
+ * Convert base64 cursor to string
+ * @param  {string} str
+ */
 export const cursorToString = (cursor: string) => {
   return Buffer.from(cursor, 'base64').toString('binary');
 };
 
+/**
+ * Wrap node to cursor object
+ * @param  {Node} node
+ * @returns string
+ */
 export const nodeToEdge = (node: Node): { node: Node; cursor: string } => {
   return {
     node,
@@ -17,7 +35,7 @@ export const nodesListToEdges = (nodeList: Node[]) => {
   return nodeList.map(node => nodeToEdge(node));
 };
 
-const buildCursorBundle = (props: IProps): ICursorBundle => {
+const buildCursorConnection = (props: IProps): ICursorConnection => {
   const { nodes, totalCount } = props;
 
   const cursor = {
@@ -34,18 +52,61 @@ const buildCursorBundle = (props: IProps): ICursorBundle => {
   return cursor;
 };
 
+const buildQueryFilter = <TFilter extends TFilterDefaults = TFilterDefaults, TArgs extends TArgsDefaults = {}>(
+  args: TArgs,
+): TFilter & TFilterDefaults => {
+  const { first, last, after, before, orderBy } = args;
+
+  // combine filter
+  const filter = {
+    limit: first !== undefined ? first : last,
+    orderBy: [
+      {
+        column: 'cursor',
+        order: after !== undefined ? IDirectionRange.ASC : IDirectionRange.DESC,
+      },
+    ],
+    where: {},
+  } as TFilter;
+
+  if (after !== undefined) {
+    filter.after = Number(cursorToString(after));
+  }
+
+  if (before !== undefined) {
+    filter.before = Number(cursorToString(before));
+  }
+
+  if (typeof orderBy === 'object') {
+    filter.orderBy.unshift({
+      column: orderBy.field,
+      order: orderBy.direction,
+    });
+  }
+
+  return filter;
+};
+
 interface IProps {
   totalCount: number;
   limit: number;
   nodes: Node[];
 }
 
-export interface ICursorBundle {
+/**
+ * GraphQL Cursor connection
+ * @see https://facebook.github.io/relay/graphql/connections.htm
+ */
+export interface ICursorConnection {
   edges: Edge[];
   pageInfo: IPageInfo;
   totalCount: number;
 }
 
+/**
+ * GraphQL PageInfo
+ * @see https://facebook.github.io/relay/graphql/connections.htm#sec-undefined.PageInfo
+ */
 export interface IPageInfo {
   startCursor?: string;
   endCursor?: string;
@@ -53,15 +114,50 @@ export interface IPageInfo {
   hasNextPage: boolean;
 }
 
+/**
+ * GraphQL Edge type
+ * @see https://facebook.github.io/relay/graphql/connections.htm#sec-Edge-Types
+ */
 export interface Edge {
   cursor: string;
   node: Node;
 }
 
+/**
+ * GraphQL Node type
+ * @see https://facebook.github.io/relay/graphql/connections.htm#sec-Node
+ */
 export interface Node {
   cursor: number;
   [key: string]: any;
 }
 
-export default buildCursorBundle;
-export { buildCursorBundle };
+export interface IListResponse<TNode> {
+  totalCount: number;
+  nodes: TNode[];
+  limit: number;
+}
+
+interface TArgsDefaults {
+  first?: number;
+  last?: number;
+  after?: string;
+  before?: string;
+  orderBy?: {
+    field: string;
+    direction: IDirectionRange;
+  };
+}
+
+interface TFilterDefaults {
+  after?: number;
+  before?: number;
+  limit?: number;
+  orderBy?: Array<{
+    column: string;
+    order: IDirectionRange;
+  }>;
+  where?: {};
+}
+
+export { buildCursorConnection, buildQueryFilter };
