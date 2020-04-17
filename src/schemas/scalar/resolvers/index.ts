09e945d8ede3c9611c6fdbfcfeb4bc24c428134f
Vasily Novosad <delhsmail@gmail.com>
2020-04-17 19:02:29 +0500

    Scalar «Time» was added

--- src/schemas/scalar/resolvers/index.ts
+++ src/schemas/scalar/resolvers/index.ts
@@ -1,11 +1,13 @@
 import DateTime from './DateTime';
 import EmailAddress from './EmailAddress';
 import Money from './Money';
+import Time from './Time';
 import URL from './URL';
 
 export {
   DateTime,
   EmailAddress,
   Money,
+  Time,
   URL,
 };
