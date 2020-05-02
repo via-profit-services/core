a2740493088dd46694eeb01ffc330921822652fd
Vasily Novosad <delhsmail@gmail.com>
2020-05-02 19:15:29 +0500

    Build

---
+++ dist/schemas/scalar/resolvers/JSON.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Original code here https://github.com/Urigo/graphql-scalars/blob/master/src/resolvers/JSON.ts
+ */
+import { GraphQLScalarType } from 'graphql';
+export declare const JSON: GraphQLScalarType;
+export declare const JSONObject: GraphQLScalarType;
