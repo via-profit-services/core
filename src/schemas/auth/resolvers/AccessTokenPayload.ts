89809eafecdc00f7b44e9fff0a961239373f0677
Vasily Novosad <delhsmail@gmail.com>
2020-06-06 03:50:48 +0500

    Build

--- src/schemas/auth/resolvers/AccessTokenPayload.ts
+++ src/schemas/auth/resolvers/AccessTokenPayload.ts
@@ -18,6 +18,9 @@ const accessTokenPayloadResolver = new Proxy<TAccountResolver>({
   get: (target, prop: keyof IAccessToken['payload']) => {
     const resolver: IFieldResolver<IParent, IContext> = async (parent, args, context) => {
       const token = (parent.id && parent.id !== '') ? parent : context.token;
+      console.log('Access token resolver');
+      console.log('parent', parent);
+      console.log('context.token', context.token);
       return token[prop];
     };
     return resolver;
