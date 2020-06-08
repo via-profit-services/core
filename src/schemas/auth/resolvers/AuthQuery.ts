00975236cc9ad65919dc3abe46c5c42e2eb549fb
Vasily Novosad <delhsmail@gmail.com>
2020-06-08 09:32:23 +0500

    Build

--- src/schemas/auth/resolvers/AuthQuery.ts
+++ src/schemas/auth/resolvers/AuthQuery.ts
@@ -1,9 +1,19 @@
 import { IResolverObject } from 'graphql-tools';
 
 import { IContext } from '../../../types';
+import AuthService from '../service';
 
 const authQueryResolver: IResolverObject<any, IContext> = {
-  token: () => ({}),
+  validateToken: async (parent, args: IValidateTokenArgs, context) => {
+    const { token } = args;
+    const authService = new AuthService({ context });
+    const tokenPayload = authService.verifyToken(token);
+    return tokenPayload;
+  },
 };
+
+interface IValidateTokenArgs {
+  token: string;
+}
 
 export default authQueryResolver;
