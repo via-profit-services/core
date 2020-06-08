48ef591eed0f29c5362fe0ae22acae622ae886c9
Vasily Novosad <delhsmail@gmail.com>
2020-06-08 15:52:11 +0500

    Build

--- src/schemas/accounts/resolvers/Account.ts
+++ src/schemas/accounts/resolvers/Account.ts
@@ -22,10 +22,8 @@ const accountResolver = new Proxy<TAccountResolver>({
   get: (target, prop: keyof IAccount) => {
     const resolver: IFieldResolver<IParent, IContext> = async (parent, args, context) => {
       const { id } = parent;
-      console.log('get ID', id);
       const loaders = createDataloaders(context);
       const account = await loaders.accounts.load(id);
-      console.log('loaded account', account);
       return account[prop];
     };
     return resolver;
