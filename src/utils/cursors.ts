03462e957fee7a1e5354ccf87f96e0eda9324dac
Vasily Novosad <delhsmail@gmail.com>
2021-06-22 10:11:36 +0500

    Fix cursor paginator when passed «last» and «before»

--- src/utils/cursors.ts
+++ src/utils/cursors.ts
@@ -1,22 +1,24 @@
-import type { StringToCursor, CursorToString, MakeNodeCursor, GetCursorPayload, BuildCursorConnection } from '@via-profit-services/core';
+import type {
+  StringToCursor,
+  CursorToString,
+  MakeNodeCursor,
+  GetCursorPayload,
+  BuildCursorConnection,
+} from '@via-profit-services/core';
 
 import ServerError from '../errorHandlers/ServerError';
 import { nodeToEdge } from './nodes';
 
+export const stringToCursor: StringToCursor = str =>
+  Buffer.from(String(str), 'utf8').toString('base64');
 
-export const stringToCursor: StringToCursor = (str) => Buffer.from(String(str), 'utf8').toString('base64');
+export const cursorToString: CursorToString = cursor =>
+  Buffer.from(cursor, 'base64').toString('utf8');
 
+export const makeNodeCursor: MakeNodeCursor = (cursorName, cursorPayload) =>
+  stringToCursor([JSON.stringify(cursorPayload), cursorName].join('---'));
 
-export const cursorToString: CursorToString = (cursor) => Buffer.from(cursor, 'base64').toString('utf8');
-
-
-export const makeNodeCursor: MakeNodeCursor = (cursorName, cursorPayload) => stringToCursor([
-  JSON.stringify(cursorPayload),
-  cursorName,
-].join('---'));
-
-
-export const getCursorPayload: GetCursorPayload = (cursor) => {
+export const getCursorPayload: GetCursorPayload = cursor => {
   try {
     const cursorPayload = cursorToString(cursor).split('---')[0] || '';
 
@@ -26,28 +28,21 @@ export const getCursorPayload: GetCursorPayload = (cursor) => {
   }
 };
 
-
 export const buildCursorConnection: BuildCursorConnection = (props, cursorName) => {
-  const {
-    nodes,
-    totalCount,
-    offset,
-    limit,
-    where,
-    orderBy,
-  } = props;
+  const { nodes, totalCount, offset, limit, where, revert, orderBy } = props;
 
-  // const edges = nodesToEdges(nodes, cursorName);
-  const edges = nodes.map((node, index) => nodeToEdge(node, cursorName, {
+  const edges = nodes.map((node, index) =>
+    nodeToEdge(node, cursorName, {
       offset: (offset || 0) + index + 1,
       limit,
       where: where || [],
       orderBy: orderBy || [],
-    }));
+    }),
+  );
   const startCursor = edges.length ? edges[0].cursor : undefined;
   const endCursor = edges.length ? edges[edges.length - 1].cursor : undefined;
-  const hasPreviousPage = (offset || 0) > 0;
-  const hasNextPage = (offset || 0) + limit < totalCount;
+  const hasPreviousPage = revert ? (offset || 0) + limit < totalCount : (offset || 0) > 0;
+  const hasNextPage = revert ? (offset || 0) > 0 : (offset || 0) + limit < totalCount;
 
   return {
     totalCount,
