37e846d8e1e2c78b64c63c5af77624fbd3ad739c
Vasily Novosad <delhsmail@gmail.com>
2021-12-30 14:35:33 +0500

    WIP

--- src/application.ts
+++ src/application.ts
@@ -147,7 +147,6 @@ const applicationFactory: ApplicationFactory = async props => {
       });
 
       if (errors) {
-        console.log(errors);
         throw new ServerError(errors, 'execute');
       }
 
