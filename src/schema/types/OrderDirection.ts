d2e56cfe816baf00db301984cc48b918ab1a1209
Vasily Novosad <delhsmail@gmail.com>
2022-01-12 11:17:06 +0500

    Description added to GraphQL type

--- src/schema/types/OrderDirection.ts
+++ src/schema/types/OrderDirection.ts
@@ -4,8 +4,14 @@ const OrderDirection = new GraphQLEnumType({
   name: 'OrderDirection',
   description: 'Standart ordering options',
   values: {
-    ASC: { value: 'ASC' },
-    DESC: { value: 'DESC' },
+    ASC: {
+      value: 'asc',
+      description: 'Sort the query results in a top to bottom style (e.g.: A->Z)',
+    },
+    DESC: {
+      value: 'desc',
+      description: 'Sort the query results in a bottom to top style (e.g.: Z->A)',
+    },
   },
 });
 
