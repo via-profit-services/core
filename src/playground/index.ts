e2fc91059f391b4305b657ca52944242845e5071
Vasily Novosad <delhsmail@gmail.com>
2023-06-26 17:35:58 +0500

    Playground

--- src/playground/index.ts
+++ src/playground/index.ts
@@ -6,9 +6,15 @@ import {
   GraphQLSchema,
   GraphQLString,
   GraphQLInt,
+  GraphQLList,
 } from 'graphql';
+import crypto from 'node:crypto';
+import fs from 'node:fs';
 
 import { graphqlHTTPFactory } from '../index';
+import FileUpload from '../schema/scalars/FileUpload';
+import path from 'node:path';
+import type { UploadedFile } from '@via-profit-services/core';
 
 const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
@@ -25,6 +31,50 @@ const schema = new GraphQLSchema({
       getFiveWithError: {
         type: new GraphQLNonNull(GraphQLInt),
         resolve: () => null, // will be error
+      },
+    }),
+  }),
+  mutation: new GraphQLObjectType({
+    name: 'Mutation',
+    fields: () => ({
+      uploadFile: {
+        type: new GraphQLNonNull(GraphQLString),
+        args: {
+          files: {
+            type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(FileUpload))),
+          },
+        },
+        resolve: async (_p, args: { files: readonly UploadedFile[] }) => {
+          const { files } = args;
+
+          const filesData = await Promise.all(files);
+
+          await filesData.reduce(async (prev, file) => {
+            await prev;
+            const { createReadStream, mimeType, capacitor } = file;
+            const readStream = createReadStream();
+
+            const distDir = path.resolve(__dirname, '../.files');
+            const name = crypto.randomUUID();
+            const ext = mimeType.replace(/^.*?\//, '');
+            const writeStream = fs.createWriteStream(`${distDir}/${name}.${ext}`);
+
+            return new Promise<void>((resolve, reject) => {
+              writeStream.on('error', err => {
+                capacitor.destroy();
+                reject(err);
+              });
+              writeStream.on('finish', () => {
+                capacitor.destroy();
+                resolve();
+              });
+
+              readStream.pipe(writeStream);
+            });
+          }, Promise.resolve());
+
+          return 'ok';
+        },
       },
     }),
   }),
